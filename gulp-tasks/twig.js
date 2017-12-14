9527ce43e63a8584a783677a4a418b5891754e6f
robineInternetJongens <robine@deinternetjongens.nl>
2017-12-14 11:09:30 +0100

    Initial commit

---
+++ gulp-tasks/twig.js
@@ -0,0 +1,44 @@
+module.exports = (GULP, PLUGINS, NODE_MODULES, REVISION) => {
+    return function (callback) {
+        var twig_options = {
+            base: process.env.SRC,
+            errorLogToConsole: true
+        }
+
+        var src = GULP.src(
+            [
+                process.env.SRC + '/**/*.twig',
+                '!' + process.env.SRC + '/**/layouts/**/*.twig',
+                '!' + process.env.SRC + '/**/partials/*.twig',
+            ])
+            .pipe(PLUGINS.plumber())
+            .pipe(PLUGINS.twig(twig_options))
+            .pipe(PLUGINS.faker())
+            .pipe(GULP.dest(process.env.DEST));
+
+        var packages = GULP.src(
+            [
+                process.env.MODULES_PATH + '/*tipi*/**/*.twig'
+            ])
+            .pipe(PLUGINS.plumber())
+            .pipe(PLUGINS.data(function (file) {
+                var data = 'data.json';
+
+                var path = NODE_MODULES.path.relative(file.cwd, file.path);
+                var extension = NODE_MODULES.path.basename(path);
+
+                var data = path.replace(extension, 'data.json');
+
+                // Check if data.json exists
+                if (!NODE_MODULES.fse.existsSync(data)) {
+                    return {};
+                }
+                return JSON.parse(NODE_MODULES.fse.readFileSync(data));
+            }))
+            .pipe(PLUGINS.twig(twig_options))
+            .pipe(PLUGINS.faker())
+            .pipe(GULP.dest(process.env.DEST + '/packages'));
+
+        return NODE_MODULES.merge(src, packages).pipe(PLUGINS.connect.reload());
+    }
+}
---
+++ gulpfile.js
@@ -0,0 +1,101 @@
+// Require Node dotenv to enable environment variables
+const ENV = require('dotenv').config();
+if (ENV.error) {
+    throw 'Cannot load the environment file at: /.env. You can create one by using the included .env.dist file.';
+}
+
+// Load Gulp
+const GULP = require('gulp');
+
+// Load all Gulp plugins dynamicly
+const PLUGINS = require('gulp-load-plugins')();
+
+const NODE_MODULES = {
+    buffer: require('vinyl-buffer'),
+    merge: require('merge-stream'),
+    runSequence: require('run-sequence'),
+    path: require('path'),
+    del: require('del'),
+    fse: require('fse')
+};
+
+// Revision timestamp of the current date in seconds
+const REVISION = new Date().getTime();
+
+// Helper function for defining tasks
+function getGulpTask(file) {
+    return require('./gulp-tasks/' + file)(GULP, PLUGINS, NODE_MODULES, REVISION);
+}
+
+// Prepare tasks
+GULP.task('clean', getGulpTask('clean'));
+GULP.task('copy', getGulpTask('copy'));
+
+// Stylesheets
+GULP.task('sass', getGulpTask('sass'));
+GULP.task('spritesmith', getGulpTask('spritesmith'));
+GULP.task('svgstore', getGulpTask('svgstore'));
+GULP.task('stylesheets', function (callback) {
+    NODE_MODULES.runSequence(
+        'spritesmith',
+        [
+            'svgstore',
+            'sass'
+        ],
+        callback
+    );
+});
+
+// Javascripts
+GULP.task('concat', getGulpTask('concat'));
+GULP.task('javascripts', function (callback) {
+    NODE_MODULES.runSequence(
+        'concat',
+        callback
+    );
+});
+// Pages
+GULP.task('twig', getGulpTask('twig'));
+GULP.task('pages', function (callback) {
+    NODE_MODULES.runSequence(
+        'twig',
+        callback
+    );
+});
+
+// Tools
+GULP.task('connect', getGulpTask('connect'));
+GULP.task('watch', getGulpTask('watch'));
+GULP.task('serve', function (callback) {
+    NODE_MODULES.runSequence(
+        'clean',
+        'copy',
+        [
+            'stylesheets',
+            'spritesmith',
+            'svgstore',
+            'javascripts',
+            'pages'
+        ],
+        [
+            'connect',
+            'watch',
+        ],
+        callback
+    );
+});
+
+GULP.task('default', function(callback) {
+    NODE_MODULES.runSequence(
+        'clean',
+        'copy',
+        [
+            'stylesheets',
+            'spritesmith',
+            'svgstore',
+            'javascripts',
+            'pages'
+        ],
+        callback
+    );
+});
